0451f58ab8386788a89b1382a9907b73ecbef57a
ducthang <plaheace9@gmail.com>
2020-02-16 16:38:46 +0700

    Js m_scroll navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
-$(document).ready(function() {
+$(function() {
+    $(window).scroll(function() {
 
-    $("#owl-demo").owlCarousel({
+        if ($(window).scrollTop() > 515) {
+            $('.fixed-colorBackground').addClass('cb-fixed');
+        } else {
+            $('.fixed-colorBackground').removeClass('cb-fixed');
 
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
-
-        items: 4,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3]
-
+        }
     });
-
 });

3e6cc6b0f55175aee3b6fff9c32ff8385186acd5
ducthang <plaheace9@gmail.com>
2020-02-16 16:55:19 +0700

    fix px

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(function() {
     $(window).scroll(function() {
 
-        if ($(window).scrollTop() > 515) {
+        if ($(window).scrollTop() > 400) {
             $('.fixed-colorBackground').addClass('cb-fixed');
         } else {
             $('.fixed-colorBackground').removeClass('cb-fixed');
 
         }
+
     });
 });

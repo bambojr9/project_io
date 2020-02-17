f52bf203fa2e782009d31687e6a3b8570ac53ade
ducthang <plaheace9@gmail.com>
2020-02-17 23:05:14 +0700

    update js btn--up

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,16 @@ $(function() {
 
         if ($(window).scrollTop() > 400) {
             $('.fixed-colorBackground').addClass('cb-fixed');
+            $('.btn--up').addClass('show');
         } else {
             $('.fixed-colorBackground').removeClass('cb-fixed');
-
+            $('.btn--up').removeClass('show');
         }
-
+    });
+    $('.btn--up').on('click', function(e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
     });
 });

d2bfd69d5b6b21e4b8f6a39743a118a43e315e84
ducthang <plaheace9@gmail.com>
2020-02-18 15:52:36 +0700

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+    //scroll change& add class
     $(window).scroll(function() {
 
         if ($(window).scrollTop() > 400) {
@@ -9,10 +10,24 @@ $(function() {
             $('.btn--up').removeClass('show');
         }
     });
+    // scroll top
+
     $('.btn--up').on('click', function(e) {
         e.preventDefault();
         $('html,body').animate({
             scrollTop: 0
         }, 700);
     });
+
+    // scroll section
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(e) {
+            var data_id = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(data_id).offset().top
+            }, '500');
+        });
+    //
 });
